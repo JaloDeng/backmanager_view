0f679bf9fcf9a4996f26b4fe50d50f72e8963254
JaloDeng <651379728@qq.com>
2019-05-21 09:57:03 +0800

    创建登陆页面界面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: () => import('@/components/HelloWorld')
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: () => import('@/components/Home')
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: () => import('@/components/Login')
     }
   ]
 })
