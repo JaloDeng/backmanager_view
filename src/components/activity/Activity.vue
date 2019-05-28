a75e1f8f1d95ae516865648bc633ee99b3d7e0c3
JaloDeng <651379728@qq.com>
2019-05-28 17:24:22 +0800

    优化活动页面搜索和分页功能

--- src/components/activity/Activity.vue
+++ src/components/activity/Activity.vue
@@ -4,7 +4,7 @@
       <el-header style="padding: 0px;display:flex;justify-content:space-between;align-items: center">
         <div style="display: inline">
           <el-input placeholder="" clearable @change="keywordsChange" style="width: 300px;margin: 0px;padding: 0px;"  size="mini"
-            @keyup.enter.native="search" prefix-icon="el-icon-search" v-model="keywords"></el-input>
+            @keyup.enter.native="search" prefix-icon="el-icon-search" v-model="activity.title"></el-input>
           <el-button type="primary" size="mini" style="margin-left: 5px" icon="el-icon-search" @click="search"></el-button>
         </div>
         <div style="margin-left: 5px;margin-right: 20px;display: inline">
@@ -16,17 +16,19 @@
           <el-table :data="activities" v-loading="tableLoading" size="mini" style="width: 100%" border="0">
             <el-table-column type="selection" width="30"></el-table-column>
             <el-table-column prop="title" label="主题"></el-table-column>
-            <el-table-column prop="status" label="状态"></el-table-column>
+            <el-table-column prop="statusName" label="状态"></el-table-column>
             <el-table-column prop="sponsor" label="供应商"></el-table-column>
             <el-table-column prop="price" label="价格"></el-table-column>
           </el-table>
-          <div style="display:flex; justify-content:space-between;">
-            <el-pagination :page-size="10" :current-page="currentPage" @current-change="currentChange"
-              layout="total, prev, pager, next" :total="totalCount"></el-pagination>
+          <div style="justify-content:space-between;">
+            <el-pagination background :page-size="10" :current-page="activity.page" @current-change="currentChange"
+              layout="total, prev, pager, next" :total="totalCount" style="text-align:center"></el-pagination>
           </div>
         </div>
       </el-main>
     </el-container>
+    <el-form :model="activity" :rules="rules" ref="addActivityForm" style="margin: 0px;padding: 0px;">
+    </el-form>
   </div>
 </template>
 
@@ -36,26 +38,26 @@ export default {
     return {
       activities: [],
       activity: {
-        page: '',
+        title: '',
+        page: 1,
         size: 10
       },
-      currentPage: 1,
-      keywords: '',
       tableLoading: false,
       totalCount: 0
     }
   },
   methods: {
     currentChange (currentChange) {
-      this.currentPage = currentChange
+      this.activity.page = currentChange
       this.loadActivities()
     },
     search () {
+      this.activity.page = 1
       this.loadActivities()
     },
     keywordsChange (val) {
       if (val === '') {
-        this.loadActivities()
+        this.search()
       }
     },
     showAddActivityView () {
@@ -69,6 +71,7 @@ export default {
         if (resp && resp.status === 200) {
           var data = resp.data
           _this.activities = data.data
+          _this.totalCount = data.count
         }
       })
     }
